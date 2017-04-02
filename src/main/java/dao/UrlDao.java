2623b355c436132643c7555d57c4de349e261de3
gasanov <gasanov.sh@gmail.com>
2017-04-02 14:41:34 +0200

    addind jandex dependency in pom.xml for hibernate

--- src/main/java/dao/UrlDao.java
+++ src/main/java/dao/UrlDao.java
@@ -30,7 +30,7 @@ public class UrlDao {
 	@PostConstruct
 	public void init() {
 		conn = db.getConnection();
-		// sessionFactory = HibernateUtil.getSessionFactory();
+		sessionFactory = HibernateUtil.getSessionFactory();
 	}
 
 	@PreDestroy
@@ -40,64 +40,64 @@ public class UrlDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		// sessionFactory.close();
+		 sessionFactory.close();
 	}
 
-	// public boolean putUrl(UrlBean bean) {
-	// try {
-	// session = sessionFactory.getCurrentSession();
-	// session.beginTransaction();
-	// session.save(bean);
-	// session.getTransaction().commit();
-	// } catch (HibernateException e) {
-	// System.out.println(e.getMessage());
-	// session.close();
-	// return false;
-	// }
-	// session.close();
-	// return true;
-	// }
+	 public boolean putUrl(UrlBean bean) {
+	 try {
+	 session = sessionFactory.getCurrentSession();
+	 session.beginTransaction();
+	 session.save(bean);
+	 session.getTransaction().commit();
+	 } catch (HibernateException e) {
+	 System.out.println(e.getMessage());
+//	 session.close();
+	 return false;
+	 }
+//	 session.close();
+	 return true;
+	 }
 
-	public boolean putUrl(UrlBean bean) {
-		try {
-			PreparedStatement pst = conn.prepareStatement(
-					"INSERT INTO url (shortUrl, longUrl, createDate, expireDate, password, nbClicks, userId, generic) VALUES (?,?,?,?,?,?,?,?)");
-
-			pst.setString(1, bean.getShortUrl());
-			pst.setString(2, bean.getLongUrl());
-			pst.setTimestamp(3, bean.getCreateDate());
-			if (bean.getExpireDate() != null) {
-				pst.setTimestamp(4, bean.getExpireDate());
-			} else {
-				pst.setNull(4, java.sql.Types.TIMESTAMP);
-			}
-			if (bean.getPassword() != null) {
-				pst.setString(5, bean.getPassword());
-			} else {
-				pst.setNull(5, java.sql.Types.VARCHAR);
-			}
-			if (bean.getNbClicks() != null) {
-				pst.setInt(6, bean.getNbClicks());
-			} else {
-				pst.setNull(6, java.sql.Types.INTEGER);
-			}
-			if (bean.getUserId() != null) {
-				pst.setInt(7, bean.getUserId());
-			} else {
-				pst.setNull(7, java.sql.Types.INTEGER);
-			}
-			pst.setBoolean(8, bean.isGeneric());
-
-			pst.executeUpdate();
-
-			pst.close();
-			conn.close();
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+//	public boolean putUrl(UrlBean bean) {
+//		try {
+//			PreparedStatement pst = conn.prepareStatement(
+//					"INSERT INTO url (shortUrl, longUrl, createDate, expireDate, password, nbClicks, userId, generic) VALUES (?,?,?,?,?,?,?,?)");
+//
+//			pst.setString(1, bean.getShortUrl());
+//			pst.setString(2, bean.getLongUrl());
+//			pst.setTimestamp(3, bean.getCreateDate());
+//			if (bean.getExpireDate() != null) {
+//				pst.setTimestamp(4, bean.getExpireDate());
+//			} else {
+//				pst.setNull(4, java.sql.Types.TIMESTAMP);
+//			}
+//			if (bean.getPassword() != null) {
+//				pst.setString(5, bean.getPassword());
+//			} else {
+//				pst.setNull(5, java.sql.Types.VARCHAR);
+//			}
+//			if (bean.getNbClicks() != null) {
+//				pst.setInt(6, bean.getNbClicks());
+//			} else {
+//				pst.setNull(6, java.sql.Types.INTEGER);
+//			}
+//			if (bean.getUserId() != null) {
+//				pst.setInt(7, bean.getUserId());
+//			} else {
+//				pst.setNull(7, java.sql.Types.INTEGER);
+//			}
+//			pst.setBoolean(8, bean.isGeneric());
+//
+//			pst.executeUpdate();
+//
+//			pst.close();
+//			conn.close();
+//			return true;
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return false;
+//		}
+//	}
 
 	public UrlBean getUrl(String shortUrl) {
 		PreparedStatement pst;
